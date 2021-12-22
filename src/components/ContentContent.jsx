806cf5dbcacbe579ed68255324c7f0c36695610e
tomus151 <ciasteczkowy15@gmail.com>
2021-12-22 10:11:20 +0100

    added images pages, added banners iframes

--- src/components/ContentContent.jsx
+++ src/components/ContentContent.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import '../styles/ContentContent.css'
+import '../styles/ContentContent.css';
+import WorkExpirience from './WorkExpirience'
 const ContentContent = (props) => {
     const { isActive } = props
     let classNames = isActive ? ' active' : '';
 
     return (
         <div className={`list-item-content-container${classNames}`}>
-            aaaaaaaaaa <br />
-            asdsadasd <br />
-            asdasdsadasdasdasd <br />
-            asdsadasdasdasdasasd <br />
-            asdsadasdas <br />
-            divsa <br />
-            dsadasd <br />
-            a <br />
-            sad <br />
-            as <br />
-            d <br />
-            sa <br />
-            d <br />
-            saddsa <br />
+            <WorkExpirience />
         </div>
     );
 }
--- src/components/WorkExpirience.jsx
+++ src/components/WorkExpirience.jsx
@@ -1,9 +1,31 @@
-const WorkExpirience = (props) => {
-    return (
-        <div>
-            <h3></h3>
-        </div>
-    );
+import Raect, { PureComponent } from 'react';
+import { default as data } from '../data/expirience.json';
+import '../styles/WorkExpirience.css';
+class WorkExpirience extends PureComponent {
+    render() {
+        console.log(data.expirience)
+        const bannersSpace = data.expirience.rtbhouse.banners.map(item => {
+            const itemArrayUrls = item.url.map(url => <iframe src={url} loading='lazy' title="1"></iframe>);
+            return (
+                <div className="banner-container">
+                    <h4>{item.name}</h4>
+                    {itemArrayUrls}
+                </div>
+
+            )
+        })
+        return (
+            <>
+                <div class="rtbhouse-container">
+                    {bannersSpace}
+                </div>
+                <div class="redingo-container">
+
+                </div>
+            </>
+        );
+    }
+
 }
 
 export default WorkExpirience;
