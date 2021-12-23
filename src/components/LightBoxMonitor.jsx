dd235487629197a6c86c09edd3af93354a7f3126
tomus151 <ciasteczkowy15@gmail.com>
2021-12-23 22:59:35 +0100

    changed scrollbars

--- src/components/LightBoxMonitor.jsx
+++ src/components/LightBoxMonitor.jsx
@@ -2,7 +2,10 @@ import LightBox from './LightBox';
 import '../styles/LightBoxMonitor.css';
 import monitoImage from '../images/screen.svg';
 const LightBoxMonitor = ({ name, click }) => {
-    const handleChangeLightBoxImage = () => { return }
+    const handleChangeLightBoxImage = (e) => {
+        e.stopPropagation()
+        return
+    }
     return (
         <LightBox click={click}>
             <div className="lightbox-monitor-container">
