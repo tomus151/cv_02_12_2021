7b99dbfcca0d97bedf0962a51b2de50adea3b987
tomus151 <ciasteczkowy15@gmail.com>
2021-12-24 11:05:35 +0100

    lightbox monitor added:images change imageFunction

--- src/components/LightBoxMonitor.jsx
+++ src/components/LightBoxMonitor.jsx
@@ -1,16 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import LightBox from './LightBox';
 import '../styles/LightBoxMonitor.css';
 import monitoImage from '../images/screen.svg';
+import imagesLibrary from './imagesLibrary';
 const LightBoxMonitor = ({ name, click }) => {
+    const [imagesArray, setImagesArray] = useState([])
+    const [activeImage, setActiveImage] = useState(0)
+    useEffect(() => {
+        const changedName = name.toLowerCase().replace(/[-.]/g, '')
+        console.log(changedName)
+        debugger;
+        setImagesArray(imagesLibrary[changedName])
+    }, [name])
     const handleChangeLightBoxImage = (e) => {
         e.stopPropagation()
-        return
+        if (activeImage < imagesArray.length - 1) setActiveImage(prevValue => prevValue + 1)
+        else setActiveImage(0);
     }
+    const imagesGenerator = imagesArray.map((imageUrl, index) => (
+        <img
+            key={"lightboxmonitor" + Math.floor(Math.random() * 10000000)}
+            className={"lightbox-monitor-image-item lightbox-monitor-image-item-" + index + (index === activeImage ? " active" : "")}
+            src={imageUrl}
+            alt={`${name}-page`}
+        />
+    ))
     return (
         <LightBox click={click}>
             <div className="lightbox-monitor-container">
                 <img className="lightbox-monitor-image" src={monitoImage} alt="monitor" />
-                <div className="lightbox-monitor-images-container" onClick={handleChangeLightBoxImage}></div>
+                <div className="lightbox-monitor-images-container" onClick={handleChangeLightBoxImage}>
+                    {imagesGenerator}
+                </div>
             </div>
         </LightBox>
     );
---
+++ src/components/imagesLibrary.jsx
@@ -0,0 +1,27 @@
+import biomedpharmaImage1 from '../images/pages/normalPages/biomedpharma/1.png';
+import biomedpharmaImage2 from '../images/pages/normalPages/biomedpharma/2.png';
+import biomedpharmaImage3 from '../images/pages/normalPages/biomedpharma/3.png';
+import euroklimaImage1 from '../images/pages/normalPages/euroklima/1.png';
+import euroklimaImage2 from '../images/pages/normalPages/euroklima/2.png';
+import euroklimaImage3 from '../images/pages/normalPages/euroklima/3.png';
+import kitleplImage1 from '../images/pages/normalPages/kitlepl/1.png';
+import kitleplImage2 from '../images/pages/normalPages/kitlepl/2.png';
+import kitleplImage3 from '../images/pages/normalPages/kitlepl/3.png';
+import swdImage1 from '../images/pages/normalPages/swd/1.png';
+import swdImage2 from '../images/pages/normalPages/swd/2.png';
+import swdImage3 from '../images/pages/normalPages/swd/3.png';
+const imagesLibrary = {
+    biomedpharma: [
+        biomedpharmaImage1, biomedpharmaImage2, biomedpharmaImage3
+    ],
+    euroklima: [
+        euroklimaImage1, euroklimaImage2, euroklimaImage3
+    ],
+    kitlepl: [
+        kitleplImage1, kitleplImage2, kitleplImage3
+    ],
+    swd: [
+        swdImage1, swdImage2, swdImage3
+    ],
+}
+export default imagesLibrary;
