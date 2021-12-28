625dce357f2ce2866f66899be1f6f20a207a578f
tomus151 <ciasteczkowy15@gmail.com>
2021-12-28 12:44:10 +0100

    fix mobile touch event

--- src/components/MonitorOverlay.jsx
+++ src/components/MonitorOverlay.jsx
@@ -2,7 +2,7 @@ import overlayImage from '../images/clickHandImage.png';
 import '../styles/MonitorOverlay.css';
 const MonitorOverlay = ({ mouseenter }) => {
     return (
-        <div className="monitor-overlay-container" onMouseEnter={mouseenter}>
+        <div className="monitor-overlay-container" onMouseEnter={mouseenter} onTouchStart={mouseenter}>
             <div className="monitor-overlay-circle">
                 <img src={overlayImage} alt="hand" />
             </div>
