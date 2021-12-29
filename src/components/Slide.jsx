b9e4d5f83e9984405f4ecafa878fe61309d606b1
tomus151 <ciasteczkowy15@gmail.com>
2021-12-29 17:57:31 +0100

    changed images to webp and corection banners

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,6 +1,6 @@
 import '../styles/Slide.css'
 const Slide = ({ name, isActive }) => {
-    const image = require('../images/technologies/' + name + '.png');
+    const image = require('../images/technologies/webp/' + name + '.webp');
     return (
         <div className={`skills-image-container ${isActive ? 'active' : ''}`}>
             <img src={image.default} alt={`technology-${name}`} />
